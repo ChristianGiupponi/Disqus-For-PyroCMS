d28015a77931d37cee9fd3f21f1f5c2a81d9ca48
Christian Giupponi <christian.giupponi@stilogo.it>
2013-03-23 18:47:17 +0100

    set height for the admin area module
    now the disqus page is visibe

--- views/admin/index.php
+++ views/admin/index.php
@@ -4,6 +4,6 @@
 
 <section class="item">
 	
-	<iframe width="100%" height="100%" scrolling="auto" frameborder="0" src="http://<?php echo $short_name; ?>.disqus.com/admin"></iframe>
+	<iframe width="100%" style="height: 1000px;" scrolling="auto" frameborder="0" src="http://<?php echo $short_name; ?>.disqus.com/admin"></iframe>
 	
 </section>
